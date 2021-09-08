40943fca6935dce616c746a9512fa74512894a9b
julia <60546384+juliaowo@users.noreply.github.com>
2021-09-08 10:20:58 +0000

    Added Linux support and cool color gaming

--- amogus.py
+++ amogus.py
@@ -1,12 +1,18 @@
-# -*- coding: utf-8 -*-
 from urllib.parse import unquote, urlparse
 from pathlib import PurePosixPath
 import requests, re, os, shutil, time
+from colorama import init
+from termcolor import colored
+
+
 os.system('cls' if os.name == 'nt' else 'clear')
-logo="\n  __ _ _ __ ___   ___   __ _ _   _ ___ \n / _` | '_ ` _ \ / _ \ / _` | | | / __|\n| (_| | | | | | | (_) | (_| | |_| \__ \ \n \__,_|_| |_| |_|\___/ \__, |\__,_|___/\nRepo:                   __/ |Ver: 0.0.5\ngithub.com/R3AP3/amogus|___/           \n                          "
-print(logo)
-while True:
-    val = input("Apple Music URL: ")
+
+init()
+
+logo="\n  __ _ _ __ ___   ___   __ _ _   _ ___ \n / _` | '_ ` _ \ / _ \ / _` | | | / __|\n| (_| | | | | | | (_) | (_| | |_| \__ \ \n \__,_|_| |_| |_|\___/ \__, |\__,_|___/\nRepo:                   __/ |Ver: 1.8.7\ngithub.com/R3AP3/amogus|___/           \n                          "
+print(colored(logo, 'green'))
+while True: 
+    val = input(colored("Apple Music URL: ", 'magenta'))
     if "exit" in val or "Exit" in val:
         os.system('cls' if os.name == 'nt' else 'clear')
         exit()
@@ -36,34 +42,56 @@ while True:
     jpg__url = re.sub(regex, unc_jpg, art_url, 1)
     webp_url = re.sub(regex, cp_webp, art_url, 1)
 
-    #path = '/home/$USER/CoverArtworks/'
-    img_dir = os.path.join('C:\\','Users',os.getlogin(), 'Pictures', 'CoverArtworks')
-    path = os.path.join('C:\\','Users',os.getlogin(), 'Pictures', 'CoverArtworks', album_name)
-    
-    print(f"Saving to: {path}\n")
-    try:
-        os.mkdir(f'{path}')
-    except:
-        print('Missing Directory... Creating it rn...\n')
-        os.mkdir(f'{img_dir}')
-        os.mkdir(f'{path}')
-    png_response = requests.get(png__url, stream=True)
-    with open(f'{path}\\{album_name}.png', 'wb') as out_file:
-        shutil.copyfileobj(png_response.raw, out_file)
-    del png_response
-    print("Best Quality     PNG     Done")
+    if(os.system == 'nt'):
+        path = os.path.join('C:\\','Users',os.getlogin(), 'Pictures', 'CoverArtworks', album_name)
+    else:
+        path = '/home/' + getpass.getuser() +'/CoverArtworks/' + album_name + '/'
 
-    jpg_res = requests.get(jpg__url, stream=True)
-    with open(f'{path}\\{album_name}.jpg', 'wb') as out_file:
-        shutil.copyfileobj(jpg_res.raw, out_file)
-    del jpg_res
-    print("Best Quality     JPG     Done")
+    if(os.path.exists(path)):
+        print("Path exists \n")
+    else:
+        os.mkdir(f'{path}')
+        
 
-    webp_res = requests.get(webp_url, stream=True)
-    with open(f'{path}\\{album_name}.webp', 'wb') as out_file:
-        shutil.copyfileobj(webp_res.raw, out_file)
-    del webp_res
-    print("Best Quality     WEBP    Done")
+    if(os.name == 'nt'):
+        print(f"Saving to: {path}\n")
+        png_res = requests.get(png__url, stream=True)
+        with open(f'{path}\\{album_name}.png', 'wb') as out_file:
+            shutil.copyfileobj(png_res.raw, out_file)
+        del png_res
+        print("Best Quality     PNG     Done")
+
+        jpg_res = requests.get(jpg__url, stream=True)
+        with open(f'{path}\\{album_name}.jpg', 'wb') as out_file:
+            shutil.copyfileobj(jpg_res.raw, out_file)
+        del jpg_res
+        print("Best Quality     JPG     Done")
+
+        webp_res = requests.get(webp_url, stream=True)
+        with open(f'{path}\\{album_name}.webp', 'wb') as out_file:
+            shutil.copyfileobj(webp_res.raw, out_file)
+        del webp_res
+        print("Best Quality     WEBP    Done")
+
+    else:
+        print(f"Saving to: {path}\n")
+        png_res = requests.get(png__url, stream=True)
+        with open(f'{path}{album_name}.png', 'wb') as out_file:
+            shutil.copyfileobj(png_res.raw, out_file)
+        del png_res
+        print("Best Quality     PNG     Done")
+
+        jpg_res = requests.get(jpg__url, stream=True)
+        with open(f'{path}{album_name}.jpg', 'wb') as out_file:
+            shutil.copyfileobj(jpg_res.raw, out_file)
+        del jpg_res
+        print("Best Quality     JPG     Done")
+
+        webp_res = requests.get(webp_url, stream=True)
+        with open(f'{path}{album_name}.webp', 'wb') as out_file:
+            shutil.copyfileobj(webp_res.raw, out_file)
+        del webp_res
+        print("Best Quality     WEBP    Done")
 
     print("\nSuccessfully Downloaded\n")
     time.sleep(3.0)
