e4bccf5f036496879967b29c6d16b9f089fbdaaa
reaper <89069925+R3AP3@users.noreply.github.com>
2022-01-01 19:47:16 +0100

    rework
    redid the whole thing

--- README.md
+++ README.md
@@ -1,41 +1,39 @@
-# amogus.py - Version 0.0.5
-![amogus_du_weisst_bescheid_bra](https://user-images.githubusercontent.com/89069925/132093406-b9529188-b632-46e2-b17c-49544eb52517.jpg)
-# amogus - Apple Music Hi-Res Artwork Fetcher
 
-`this is my first real python tool so sorry if its bad`
+# Thats one sussy artwork Downloader
+![amogus](https://user-images.githubusercontent.com/89069925/147856498-ce8049f1-1248-4f25-a425-da7dc9b404f7.jpg)
+## Usage:
+Download Artwork in the best possible Quality from Apple Music in JPEG (Lossy) and PNG (Lossless)
+```
+Apple Music URL: {link}
+```
 
-
-
-amogus is a Python script that if provided an Apple Music Link downloads the Highest Available Resolution cover Artwork as a .png, .jpg and .webp file. Why all three? Since they tend to differ vastly in color and/or color room. 
-
-
-## How to use:
-
-Download `amogus.py` and the `requirements.txt`, put them in a folder and cd into it. Then you do
-
+## Installation
 ```bash
+git clone https://github.com/R3AP3/amogus.git
+```
+Install Python, this should work on any version. Then install the requirements:
+```python
 pip install -r requirements.txt
 ```
 
-## Usage
-Just start the script by typing the following in the console:
+## How to use:
+Start the Script by running:
 ```python
 python amogus.py
 ```
-You will then encounter the prompt that asks you for an apple music album URL.
+Then paste in an Apple Music URL
 
-The Pictures get saved to a Directory named "CoverArtworks" in the Pictures Folder located in `C:\Users\<Username>\Pictures` or as a bookmark in Windows explorer.
+The Pictures get saved to a Directory named "Covers" in the Folder where the Script is located.
+
+  
 
 ## To-Do:
+
 - Choosing Folder to Download to
+
 - Print Resolution While Downloading
-- Print Size of Downloaded Files
-
-im open for any ideas since this is basically the thing i tried to learn python for and so far its really fun. I legit sat like 2 days on this straight up cuz is fucking knew nothing about python. 
-
 ## Screenshot:
+![grafik](https://user-images.githubusercontent.com/89069925/147856427-7653deeb-a6e8-46ae-9e4b-944897b45031.png)
 
-
-![grafik](https://user-images.githubusercontent.com/89069925/132093221-892b1151-21c4-4d21-aa7e-ad081d305c6c.png)
-## Why did i do this:
-I realized that artworks from apple sometimes differ in color so i wrote this script to just batch save them all and save me the hassle of copying the image url everytime and fiddeling around with it.
+## Contact me:
+If you have ideas for the Script or encouter any bugs message me on [Telegram](https://t.me/WH4M1).
--- amogus.py
+++ amogus.py
@@ -1,70 +1,59 @@
 # -*- coding: utf-8 -*-
 from urllib.parse import unquote, urlparse
 from pathlib import PurePosixPath
-import requests, re, os, shutil, time
+import requests, re, os, wget
 os.system('cls' if os.name == 'nt' else 'clear')
-logo="\n  __ _ _ __ ___   ___   __ _ _   _ ___ \n / _` | '_ ` _ \ / _ \ / _` | | | / __|\n| (_| | | | | | | (_) | (_| | |_| \__ \ \n \__,_|_| |_| |_|\___/ \__, |\__,_|___/\nRepo:                   __/ |Ver: 0.0.5\ngithub.com/R3AP3/amogus|___/           \n                          "
-print(logo)
-while True:
-    val = input("Apple Music URL: ")
-    if "exit" in val or "Exit" in val:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        exit()
-    else:
-        pass
+
+img_formats = ["png","jpg"]
+
+logo="""
+  __ _ _ __ ___   ___   __ _ _   _ ___ 
+ / _` | '_ ` _ \ / _ \ / _` | | | / __|
+| (_| | | | | | | (_) | (_| | |_| \__ \ 
+ \__,_|_| |_| |_|\___/ \__, |\__,_|___/
+repository:             __/ |Ver: 0.0.8
+github.com/R3AP3/amogus|___/           """
+
+def request_img(val):
     parts = PurePosixPath(unquote(urlparse(val).path)).parts
     album_id = parts[4]
-    region_code = parts[1]
+    region_code = parts [1]
 
-    response = requests.get(f'https://itunes.apple.com/lookup?id={album_id}&country={region_code}&entity=album')
+    response = requests.get((f'https://itunes.apple.com/lookup?id={album_id}&country={region_code}&entity=album'))
 
     for details in response.json()["results"]:
-        art_url = details["artworkUrl100"]
-        art_name = details["artistName"]
+        collection_id = details["collectionId"]
+        artist_id = details["artistId"]
+        artist_name = details["artistName"]
         album_name = details["collectionName"]
+        art_url = details["artworkUrl100"]
+        copy_right = details["copyright"]
+        release_date = details["releaseDate"]
         genre_name = details["primaryGenreName"]
+        
+    meta_data = f"""
+Artist:         {artist_name} [{artist_id}]
+Album:          {album_name} [{collection_id}]
+Date:           {release_date}
+Copyright:      {copy_right}
+Genre:          {genre_name}
+"""
 
-    print(f'\nArtist:     {art_name}\nAlbum:      {album_name}\nGenre:      {genre_name}\nID:         {album_id}\n\nDownloading...\n')
+    print(meta_data)
 
     regex = r"/100x100bb.jpg"
+    print(f"Downloading...:\n")
+    for i in img_formats:
+        image_url = f"/1x1sy-100.{i}"
+        hq_image = re.sub(regex, image_url, art_url, 1)
+        naming_scheme = f'Covers/{artist_name} - {album_name} [{artist_id} - {collection_id}].{i}'
+        wget.download(hq_image, naming_scheme)
+        print(f"  [{i}]  Done!")
 
-    unc_png = "/100000x100000-100.png"
-    unc_jpg = "/100000x100000-100.jpg"
-    cp_webp = "/100000x100000-100.webp"
-
-    png__url = re.sub(regex, unc_png, art_url, 1)
-    jpg__url = re.sub(regex, unc_jpg, art_url, 1)
-    webp_url = re.sub(regex, cp_webp, art_url, 1)
-
-    #path = '/home/$USER/CoverArtworks/'
-    img_dir = os.path.join('C:\\','Users',os.getlogin(), 'Pictures', 'CoverArtworks')
-    path = os.path.join('C:\\','Users',os.getlogin(), 'Pictures', 'CoverArtworks', album_name)
-    
-    print(f"Saving to: {path}\n")
-    try:
-        os.mkdir(f'{path}')
-    except:
-        print('Missing Directory... Creating it rn...\n')
-        os.mkdir(f'{img_dir}')
-        os.mkdir(f'{path}')
-    png_response = requests.get(png__url, stream=True)
-    with open(f'{path}\\{album_name}.png', 'wb') as out_file:
-        shutil.copyfileobj(png_response.raw, out_file)
-    del png_response
-    print("Best Quality     PNG     Done")
-
-    jpg_res = requests.get(jpg__url, stream=True)
-    with open(f'{path}\\{album_name}.jpg', 'wb') as out_file:
-        shutil.copyfileobj(jpg_res.raw, out_file)
-    del jpg_res
-    print("Best Quality     JPG     Done")
-
-    webp_res = requests.get(webp_url, stream=True)
-    with open(f'{path}\\{album_name}.webp', 'wb') as out_file:
-        shutil.copyfileobj(webp_res.raw, out_file)
-    del webp_res
-    print("Best Quality     WEBP    Done")
-
-    print("\nSuccessfully Downloaded\n")
-    time.sleep(3.0)
-    os.system('cls' if os.name == 'nt' else 'clear')
+print(logo)
+while True:
+    val = input("\nApple Music URL: ")
+    if 'http' in val:
+        request_img(val)
+    else:
+        print("Wrong input")
