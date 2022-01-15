f591f93af8ea2acc447052c80f87f2537b435229
reaper <89069925+R3AP3@users.noreply.github.com>
2022-01-15 20:31:22 +0100

    added new feature
    you can now download all covers of an artist via their artist url (experimental may crash)

--- amogus.py
+++ amogus.py
@@ -5,32 +5,23 @@ from pathlib import PurePosixPath
 from pymediainfo import MediaInfo
 import requests, re, os, wget
 
-logo="  __ _ _ __ ___   ___   __ _ _   _ ___ \n / _` | '_ ` _ \ / _ \ / _` | | | / __|\n| (_| | | | | | | (_) | (_| | |_| \__ \ \n \__,_|_| |_| |_|\___/ \__, |\__,_|___/\nrepository:             __/ |Ver: 0.0.9\ngithub.com/R3AP3/amogus|___/           "
+logo="  __ _ _ __ ___   ___   __ _ _   _ ___ \n / _` | '_ ` _ \ / _ \ / _` | | | / __|\n| (_| | | | | | | (_) | (_| | |_| \__ \ \n \__,_|_| |_| |_|\___/ \__, |\__,_|___/\nrepository:             __/ |Ver: 0.0.11\ngithub.com/R3AP3/amogus|___/           "
 
-os.system('cls' if os.name == 'nt' else 'clear')
-cfg = ConfigParser()
-cfg.read('config.ini')
-file_path = cfg.get('settings', 'file_path')
-img_set = cfg.get('settings', 'img_formats')
+def get_collectionids(album_id, region_code):
+    response = requests.get(f'https://itunes.apple.com/lookup?id={album_id}&country={region_code}&entity=album')
+    data = response.json()["results"]
+    idlist = []
+    for i in range(len(data)):
+        if i == 0:
+            print(data[i])
+        else:
+            dict_raw = data[i]
+            collection_id = dict_raw["collectionId"]
+            idlist.append(collection_id)
+    return idlist;
 
-if file_path == "":
-    print("Config Error: file_path")
-
-if img_set == 'normal':
-    img_formats = ["jpg","png"]
-elif img_set == 'experimental':
-    img_formats = ["jpg","png","webp","heic"]
-else:
-    print("Config Error: img_formats")
-    exit()
-
-def request_img(val):
-    url_parts = PurePosixPath(unquote(urlparse(val).path)).parts
-    album_id = url_parts[4]
-    region_code = url_parts[1]
-
-    response = requests.get((f'https://itunes.apple.com/lookup?id={album_id}&country={region_code}&entity=album'))
-
+def download_covers(album_id, region_code):
+    response = requests.get(f'https://itunes.apple.com/lookup?id={album_id}&country={region_code}&entity=album')
     for details in response.json()["results"]:
         collection_id = details["collectionId"]
         artist_id = details["artistId"]
@@ -40,7 +31,7 @@ def request_img(val):
         copy_right = details["copyright"]
         release_date = details["releaseDate"]
         genre_name = details["primaryGenreName"]
-        
+    
     meta_data = f"""
 Artist:         {artist_name} [{artist_id}]
 Album:          {album_name} [{collection_id}]
@@ -49,22 +40,45 @@ Copyright:      {copy_right}
 Genre:          {genre_name}
 """
     print(meta_data)
-    regex = r"/100x100bb.jpg"
-    print(f"Downloading...:\n")
+    regex = r"100x100bb.jpg"
+    print(f'Downloading...:\n')
     for i in img_formats:
         image_url = f"/1x1sy-100.{i}"
         image_hq = re.sub(regex, image_url, art_url, 1)
-        mi = MediaInfo.parse(image_hq)
         if i == 'jpg':
+            mi = MediaInfo.parse(image_hq)
             image_track = mi.image_tracks[0]
-            print(f"Resolution:     {image_track.width}x{image_track.height}\nBit Depth:      {image_track.bit_depth} bits\nColor Space:    {image_track.color_space}\n")
+            print(f'Resolution:     {image_track.width}x{image_track.height}\nBit Depth:      {image_track.bit_depth} bits\nColor Space:    {image_track.color_space}\n')
         wget.download(image_hq, file_path + f'{artist_name}_{album_name}-{image_track.width}x{image_track.height}_{image_track.color_space}_{image_track.bit_depth}bit.{i}')
         print(f"  [{i}]  Done!")
 
+os.system('cls' if os.name == 'nt' else 'clear')
+
+cfg = ConfigParser()
+cfg.read('config.ini')
+file_path = cfg.get('settings', 'file_path')
+img_set = cfg.get('settings', 'img_formats')
+
+if img_set == 'normal':
+    img_formats = ["jpg","png"]
+elif img_set == 'experimental':
+    img_formats = ["jpg","png","webp","heic"]
+else:
+    print("Config Error: img_formats")
+    exit()
 print(logo)
 while True:
     val = input("\nApple Music URL: ")
     if 'http' in val:
-        request_img(val)
+        url_parts = PurePosixPath(unquote(urlparse(val).path)).parts
+        album_id = url_parts[4]
+        region_code = url_parts[1]
+        if '/artist/' in val:
+            idlist = get_collectionids(album_id, region_code)
+            for i in idlist:
+                download_covers(i, region_code)
+        else:
+            download_covers(album_id, region_code)
+        
     else:
         print("Wrong input")
