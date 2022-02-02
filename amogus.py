d263cd5ec963d539835513e0c289b8d16dd9f559
reaper <89069925+R3AP3@users.noreply.github.com>
2022-02-02 09:48:34 +0100

    recoded amogus
    yeah

--- amogus.py
+++ amogus.py
@@ -1,71 +1,95 @@
-# -*- coding: utf-8 -*-
-from configparser import ConfigParser
+import requests
+from pymediainfo import MediaInfo as mi
 from urllib.parse import unquote, urlparse
 from pathlib import PurePosixPath
-from pymediainfo import MediaInfo
-import requests, re, os, wget
 
-logo="  __ _ _ __ ___   ___   __ _ _   _ ___ \n / _` | '_ ` _ \ / _ \ / _` | | | / __|\n| (_| | | | | | | (_) | (_| | |_| \__ \ \n \__,_|_| |_| |_|\___/ \__, |\__,_|___/\nrepository:             __/ |Ver: 0.0.11\ngithub.com/R3AP3/amogus|___/           "
+logo=r"""
+  __ _ _ __ ___   ___   __ _ _   _ ___ 
+ / _` | '_ ` _ \ / _ \ / _` | | | / __|
+( (_| | | | | | | (_) | (_| | |_| \__ \ 
+ \__,_|_| |_| |_|\___/ \__, |\__,_|___/
+repository:             __/ |Ver: 0.1.0
+github.com/R3AP3/amogus|___/           """
 
 def get_collectionids(album_id, region_code):
-    response = requests.get(f'https://itunes.apple.com/lookup?id={album_id}&country={region_code}&entity=album')
+    response = requests.get(f'https://itunes.apple.com/lookup?id={album_id}&country={region_code}&entity=album&limit=200&sort=recent')
     data = response.json()["results"]
     idlist = []
     for i in range(len(data)):
         if i == 0:
-            print(data[i])
+            print("Downloading Artist: " + data[i]["artistName"])
         else:
             dict_raw = data[i]
             collection_id = dict_raw["collectionId"]
             idlist.append(collection_id)
-    return idlist;
+    return idlist
 
-def download_covers(album_id, region_code):
+def parse_request(album_id, region_code):
     response = requests.get(f'https://itunes.apple.com/lookup?id={album_id}&country={region_code}&entity=album')
     for details in response.json()["results"]:
-        collection_id = details["collectionId"]
-        artist_id = details["artistId"]
-        artist_name = details["artistName"]
-        album_name = details["collectionName"]
-        art_url = details["artworkUrl100"]
-        copy_right = details["copyright"]
-        release_date = details["releaseDate"]
-        genre_name = details["primaryGenreName"]
-    
-    meta_data = f"""
-Artist:         {artist_name} [{artist_id}]
-Album:          {album_name} [{collection_id}]
-Date:           {release_date}
-Copyright:      {copy_right}
-Genre:          {genre_name}
-"""
-    print(meta_data)
-    regex = r"100x100bb.jpg"
-    print(f'Downloading...:\n')
-    for i in img_formats:
-        image_url = f"/1x1sy-100.{i}"
-        image_hq = re.sub(regex, image_url, art_url, 1)
-        if i == 'jpg':
-            mi = MediaInfo.parse(image_hq)
-            image_track = mi.image_tracks[0]
-            print(f'Resolution:     {image_track.width}x{image_track.height}\nBit Depth:      {image_track.bit_depth} bits\nColor Space:    {image_track.color_space}\n')
-        wget.download(image_hq, file_path + f'{artist_name}_{album_name}-{image_track.width}x{image_track.height}_{image_track.color_space}_{image_track.bit_depth}bit.{i}')
-        print(f"  [{i}]  Done!")
+        aid = details["artistId"]
+        cid = details["collectionId"]
+        an = details["artistName"]
+        cn = details["collectionName"]
+        au = details["artworkUrl100"]
+    return aid, cid, au, an, cn 
 
-os.system('cls' if os.name == 'nt' else 'clear')
+def download(url, filename):
+    response = requests.get(url)
+    f = open("output/" + filename, "wb")
+    f.write(response.content)
+    f.close()
 
-cfg = ConfigParser()
-cfg.read('config.ini')
-file_path = cfg.get('settings', 'file_path')
-img_set = cfg.get('settings', 'img_formats')
+def mediainfo(url):
+    data = mi.parse(url)
+    exif = data.image_tracks[0]
+    file = data.general_tracks[0]
 
-if img_set == 'normal':
-    img_formats = ["jpg","png"]
-elif img_set == 'experimental':
-    img_formats = ["jpg","png","webp","heic"]
-else:
-    print("Config Error: img_formats")
-    exit()
+    fm = exif.format
+    w = exif.width
+    h = exif.height
+    cs = exif.color_space
+    css = exif.chroma_subsampling
+    bd = exif.bit_depth
+    cm = exif.compression_mode
+    ss = exif.stream_size
+    icc = exif.colorspace_icc
+    if icc == None:
+        color_space = cs
+    else:
+        color_space = cs + "-" + icc 
+    if fm == "JPEG":
+        ext = "jpg"
+    elif fm == "PNG":
+        ext = "png"
+    fn_meta = f"{w}x{h}-[{color_space}].{ext}"
+    return fn_meta 
+
+def filename(a, c, m):
+    filename = f"{a}-{c}-{m}"
+    return filename
+
+def get_source(art):
+    p = PurePosixPath(unquote(urlparse(art).path)).parts
+    if p[9] == "source":
+        url = f"http://a1.mzstatic.com/us/r30/{p[3]}/{p[4]}/{p[5]}/{p[6]}/{p[7]}/{p[8]}/{p[9]}"
+        isSource = "SOURCE"
+    else:
+        print("Cannot get Source image. Open Issue on Github.")
+        isSource = "ENCODE"
+        url = re.sub(r"100x100bb.jpg", f"1x1sy.jpg", art)
+    return url, isSource
+
+def main(i, region_code):
+    parsed = parse_request(i, region_code)
+    cover_url = get_source(parsed[2])
+    meta = mediainfo(cover_url[0])
+    aid = str(parsed[0])
+    cid = str(parsed[1])
+    filename = str(parsed[0]) + "-" + str(parsed[1]) + "-" + meta
+    print(f"[{cover_url[1]}] {parsed[4]}")
+    download(cover_url[0], filename)
+
 print(logo)
 while True:
     val = input("\nApple Music URL: ")
@@ -76,9 +100,9 @@ while True:
         if '/artist/' in val:
             idlist = get_collectionids(album_id, region_code)
             for i in idlist:
-                download_covers(i, region_code)
+                main(i, region_code)
+
         else:
-            download_covers(album_id, region_code)
-        
+            main(album_id, region_code)
     else:
         print("Wrong input")
