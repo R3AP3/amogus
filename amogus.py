20ca25ada93799d439a2d37ccca352486105c55f
reaper <89069925+R3AP3@users.noreply.github.com>
2022-03-29 19:48:02 +0200

    Update amogus.py
    changed stuff up to more reliably download artists
    improved downloading source covers

--- amogus.py
+++ amogus.py
@@ -1,14 +1,13 @@
-import requests
-from pymediainfo import MediaInfo as mi
+import requests, os
 from urllib.parse import unquote, urlparse
 from pathlib import PurePosixPath
 
-logo=r"""
-  __ _ _ __ ___   ___   __ _ _   _ ___ 
+logo = r"""
+  __ _ _ __ ___   ___   __ _ _   _ ___
  / _` | '_ ` _ \ / _ \ / _` | | | / __|
-( (_| | | | | | | (_) | (_| | |_| \__ \ 
+( (_| | | | | | | (_) | (_| | |_| \__ \
  \__,_|_| |_| |_|\___/ \__, |\__,_|___/
-repository:             __/ |Ver: 0.1.0
+repository:             __/ |Ver: 0.2.0
 github.com/R3AP3/amogus|___/           """
 
 def get_collectionids(album_id, region_code):
@@ -26,83 +25,50 @@ def get_collectionids(album_id, region_code):
 
 def parse_request(album_id, region_code):
     response = requests.get(f'https://itunes.apple.com/lookup?id={album_id}&country={region_code}&entity=album')
-    for details in response.json()["results"]:
-        aid = details["artistId"]
-        cid = details["collectionId"]
-        an = details["artistName"]
-        cn = details["collectionName"]
-        au = details["artworkUrl100"]
-    return aid, cid, au, an, cn 
+    details = response.json()["results"][0]
+    filename = str(details["artistId"]) + " - " + str(details["collectionId"])
+    download_status = str(details["artistName"]) + " - " + str(details["collectionName"])
+    cover_url = details["artworkUrl100"]
+    return cover_url, download_status, filename
 
-def download(url, filename):
-    response = requests.get(url)
-    f = open(filename, "wb")
-    f.write(response.content)
-    f.close()
-
-def mediainfo(url):
-    data = mi.parse(url)
-    exif = data.image_tracks[0]
-    file = data.general_tracks[0]
-
-    fm = exif.format
-    w = exif.width
-    h = exif.height
-    cs = exif.color_space
-    css = exif.chroma_subsampling
-    bd = exif.bit_depth
-    cm = exif.compression_mode
-    ss = exif.stream_size
-    icc = exif.colorspace_icc
-    if icc == None:
-        color_space = cs
-    else:
-        color_space = cs + "-" + icc 
-    if fm == "JPEG":
-        ext = "jpg"
-    elif fm == "PNG":
-        ext = "png"
-    fn_meta = f"{w}x{h}-[{color_space}].{ext}"
-    return fn_meta 
-
-def filename(a, c, m):
-    filename = f"{a}-{c}-{m}"
-    return filename
+def download_picture_with_correct_extension(url, filename, download_status):
+    r = requests.get(url)
+    ext = r.headers["content-type"].split("/")[1]
+    with open(f"cover/{filename}.{ext}", "wb") as f:
+        f.write(r.content)
 
 def get_source(art):
     p = PurePosixPath(unquote(urlparse(art).path)).parts
     if p[9] == "source":
         url = f"http://a1.mzstatic.com/us/r30/{p[3]}/{p[4]}/{p[5]}/{p[6]}/{p[7]}/{p[8]}/{p[9]}"
-        isSource = "SOURCE"
-    else:
-        print("Cannot get Source image. Open Issue on Github.")
-        isSource = "ENCODE"
-        url = re.sub(r"100x100bb.jpg", f"1x1sy.jpg", art)
-    return url, isSource
+    return url
 
-def main(i, region_code):
-    parsed = parse_request(i, region_code)
-    cover_url = get_source(parsed[2])
-    meta = mediainfo(cover_url[0])
-    aid = str(parsed[0])
-    cid = str(parsed[1])
-    filename = str(parsed[0]) + "-" + str(parsed[1]) + "-" + meta
-    print(f"[{cover_url[1]}] {parsed[4]}")
-    download(cover_url[0], filename)
+def make_dir():
+    if not os.path.exists("cover"):
+        os.makedirs("cover")
 
-print(logo)
-while True:
+def routine(album_id, region_code):
+    cover_url, download_status, filename = parse_request(album_id, region_code)
+    cover_url = get_source(cover_url)
+    download_picture_with_correct_extension(cover_url, filename, download_status)
+    print(download_status)
+
+def main():
     val = input("\nApple Music URL: ")
     if 'http' in val:
         url_parts = PurePosixPath(unquote(urlparse(val).path)).parts
-        album_id = url_parts[4]
-        region_code = url_parts[1]
-        if '/artist/' in val:
-            idlist = get_collectionids(album_id, region_code)
+        if url_parts[2] == "artist":
+            idlist = get_collectionids(url_parts[4], url_parts[1])
             for i in idlist:
-                main(i, region_code)
-
+                routine(i, url_parts[1])
         else:
-            main(album_id, region_code)
+            routine(url_parts[4], url_parts[1])
     else:
         print("Wrong input")
+
+print(logo)
+make_dir()
+
+while True:
+    main()
+
