a017a684d23fd01f252b03bc1dc69136e111aa8d
reaper <89069925+R3AP3@users.noreply.github.com>
2022-04-12 07:33:42 +0200

    Updated Amogus
    Added argparse and search function

--- README.md
+++ README.md
@@ -2,10 +2,27 @@
 ![amogus](https://user-images.githubusercontent.com/89069925/147856498-ce8049f1-1248-4f25-a425-da7dc9b404f7.jpg)
 
 ## Usage:
-Download the source images of Album Artworks from Apple Music
+
+Either input a `music.apple.com` url or specify a Artist or Album search query. 
+
 ```
-Apple Music URL: {link}
+usage: amogus.py [-h] [-s] [-a] [-r REGION_CODE] INPUT
+
+Downloads Album Covers as their Source File from the Itunes Store
+
+positional arguments:
+  INPUT                 Apple Music URL or Album/Artist name
+
+options:
+  -h, --help            show this help message and exit
+  -s, --search          search for an album
+  -a, --artist          search for an artist
+  -r REGION_CODE, --region REGION_CODE
+                        specify a custom region (default is 'us', see all available region codes: https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2)
 ```
+Use the search by typing `--search` and add `--artist` to download all album covers of the desired artist.
+
+Specifying a region code is optional.
 
 ### Installing Amogus:
 
@@ -13,17 +30,7 @@ Apple Music URL: {link}
 git clone https://github.com/R3AP3/amogus.git
 ```
 Install Python, this should work on any version. Then install the requirements:
-```python
+```bash
 pip install -r requirements.txt
 ```
-## How to use:
 
-Start the Script by running:
-```
-python amogus.py
-```
-Then paste in an Apple Music URL (artist or album)
-
-The Pictures get saved to the output folder.
-## Screenshot:
-![Screenshot](https://user-images.githubusercontent.com/89069925/152122393-3b2b71ff-b5df-4050-94d0-61680f979ab7.png)
--- amogus.py
+++ amogus.py
@@ -1,4 +1,4 @@
-import requests, os
+import requests, os, argparse
 from urllib.parse import unquote, urlparse
 from pathlib import PurePosixPath
 
@@ -7,8 +7,15 @@ logo = r"""
  / _` | '_ ` _ \ / _ \ / _` | | | / __|
 ( (_| | | | | | | (_) | (_| | |_| \__ \
  \__,_|_| |_| |_|\___/ \__, |\__,_|___/
-repository:             __/ |Ver: 0.2.0
-github.com/R3AP3/amogus|___/           """
+repository:             __/ |Ver: 0.3.0
+github.com/R3AP3/amogus|___/           
+"""
+
+parser = argparse.ArgumentParser(description="Downloads Album Covers as their Source File from the Itunes Store")
+parser.add_argument("-s", "--search", dest='album_search', help="search for an album", action="store_true")
+parser.add_argument("-a", "--artist", dest='artist_search', help="search for an artist", action="store_true")
+parser.add_argument("-r", "--region", type=str, dest='region_code', help="specify a custom region (default is 'us', see all available region codes: https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2)", default="us", metavar="REGION_CODE")
+parser.add_argument("input", type=str, help="Apple Music URL or Album/Artist name", metavar="INPUT")
 
 def get_collectionids(album_id, region_code):
     response = requests.get(f'https://itunes.apple.com/lookup?id={album_id}&country={region_code}&entity=album&limit=200&sort=recent')
@@ -34,6 +41,8 @@ def parse_request(album_id, region_code):
 def download_picture_with_correct_extension(url, filename, download_status):
     r = requests.get(url)
     ext = r.headers["content-type"].split("/")[1]
+    if ext == "jpeg":
+        ext = "jpg"
     with open(f"cover/{filename}.{ext}", "wb") as f:
         f.write(r.content)
 
@@ -50,12 +59,31 @@ def make_dir():
 def routine(album_id, region_code):
     cover_url, download_status, filename = parse_request(album_id, region_code)
     cover_url = get_source(cover_url)
-    download_picture_with_correct_extension(cover_url, filename, download_status)
     print(download_status)
+    download_picture_with_correct_extension(cover_url, filename, download_status)
+    
 
-def main():
-    val = input("\nApple Music URL: ")
-    if 'http' in val:
+def search_query_itunes_artist(query, region_code):
+    response = requests.get(f'https://itunes.apple.com/search?term={query}&entity=musicArtist&limit=25')
+    data = response.json()["results"]
+    for i in range(len(data)):
+        print(f"[{i+1}] {data[i]['artistName']}")
+    album_id = data[int(input("\nChoose the Artist to download: "))-1]["artistId"]
+    idlist = get_collectionids(album_id, region_code)
+    for i in idlist:
+        routine(i, region_code)
+
+def search_query_itunes_album(query, region_code):
+    response = requests.get(f'https://itunes.apple.com/search?term={query}&entity=album&limit=25')
+    data = response.json()["results"]
+    for i in range(len(data)):
+        print(f"[{i+1}] {data[i]['artistName']} - {data[i]['collectionName']}")
+    album_id = data[int(input("\nChoose the album to download: "))-1]["collectionId"]
+    print(album_id)
+    routine(album_id, region_code)
+
+def main(val):
+    if 'https://music.apple.com' in val:
         url_parts = PurePosixPath(unquote(urlparse(val).path)).parts
         if url_parts[2] == "artist":
             idlist = get_collectionids(url_parts[4], url_parts[1])
@@ -63,12 +91,20 @@ def main():
                 routine(i, url_parts[1])
         else:
             routine(url_parts[4], url_parts[1])
-    else:
-        print("Wrong input")
 
 print(logo)
 make_dir()
 
-while True:
-    main()
+args = parser.parse_args()
 
+if args.album_search == True:
+    if args.region_code == None:
+        region_code = "us"
+    else:
+        region_code = args.region_code
+    if args.artist_search == False:
+        search_query_itunes_album(args.input, region_code)
+    elif args.artist_search == True:
+        search_query_itunes_artist(args.input, region_code)
+elif args.album_search == False:
+    main(args.input)
